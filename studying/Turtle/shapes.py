330a7fcad151664413b64c84dd0e4ac5698c4f14
Rada Telyukova <radatelyukova@gmail.com>
2017-01-29 10:21:01 +0300

    colors workaround

--- studying/Turtle/shapes.py
+++ studying/Turtle/shapes.py
@@ -20,8 +20,8 @@ t.pensize(5)
 t.speed(2)
 t.shape('turtle')
 
-shapes=['Точка', 'Линия', 'Треугольник', 'Квадрат', 'Пятиугольник', 'Шестиугольник', 'Семиугольник', 'Восьмиугольник', 'Девятиугольник', 'Деситиугольник']
-colors=['white', 'lime',  'red',         'yellow',  'lightblue',    'green',         'purple',         'cyan',         'olive',          'pink']
+shapes=['Точка', 'Линия',      'Треугольник', 'Квадрат', 'Пятиугольник', 'Шестиугольник', 'Семиугольник', 'Восьмиугольник', 'Девятиугольник', 'Деситиугольник']
+colors=['white', 'lightgreen',  'red',        'yellow',  'lightblue',    'green',         'purple',         'cyan',         'maroon',          'pink']
 
 # Loop: shapes
 for i in range(10):
@@ -39,7 +39,7 @@ for i in range(10):
     # Loop: sides of shape
     for j in range(i+1):
         if (i == 0):
-            print('point')
+            t.stamp()
         else:
             t.fd(side)
             t.rt(angle)
